0b9e158198471b0ff5e1512d119d8c6dec50d6f1
dpurcell <dpurcell.pd@gmail.com>
2023-09-12 12:34:27 -0400

    index view

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'jsx');
+app.set('views', `${__dirname}/views`);
+app.engine('jsx', require('express-react-views').createEngine());
 
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.render('Home');
+});
 
 app.listen(port, (req, res) => {
     console.log(`Now listening on port ${port}.`);
---
+++ views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+    const header = {
+        textAlign: "center"
+    };
+
+  return (
+    <div style={header}>LogSpace</div>
+  )
+}
+
