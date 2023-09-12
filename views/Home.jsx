0edb7ababae3ae05c461f691677f7207d09a6fc7
dpurcell <dpurcell.pd@gmail.com>
2023-09-12 15:38:53 -0400

    added form to Home.jsx

--- views/Home.jsx
+++ views/Home.jsx
@@ -6,7 +6,36 @@ export default function Home() {
     };
 
   return (
-    <div style={header}>LogSpace</div>
+    <div>
+        <h2 style={header}>LogSpace</h2>
+        <div>
+            <form action="">
+                <label htmlFor="title">Title: </label>
+                <br />
+                <input 
+                    type="text" 
+                    id='title' 
+                    name='title' 
+                    placeholder='Title your new post' 
+                />
+                <br /> <br />
+                <label htmlFor="text">Create your journal entry below:</label>
+                <br />
+                <textarea 
+                    name="text" 
+                    id="text" 
+                    cols="30" 
+                    rows="10" 
+                    placeholder='What would you like to say?'
+                >
+                </textarea>
+                <br /> <br />
+                <label htmlFor="image">Want to add an image?</label>
+                <br />
+                <input type="file" id="file" name="file" />
+            </form>            
+        </div>
+    </div>
   )
 }
 
