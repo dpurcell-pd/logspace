846ef6e9d167fca63c16310e06dadda5bd8168f3
dpurcell <dpurcell.pd@gmail.com>
2023-09-13 23:55:44 -0400

    added timestamp

--- models/post.js
+++ models/post.js
@@ -1,24 +1,27 @@
-const MONGOOSE = require('mongoose');
+const MONGOOSE = require("mongoose");
 const SCHEMA = MONGOOSE.Schema;
 const MODEL = MONGOOSE.model;
 
-const POST_SCHEMA = new SCHEMA({
+const POST_SCHEMA = new SCHEMA(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     text: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     image: {
-        name: String,
-        image: {
-            data: Buffer,
-            contentType: String
-        }
-    }
-});
+      name: String,
+      image: {
+        data: Buffer,
+        contentType: String,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-const POST = MODEL('Post', POST_SCHEMA);
+const POST = MODEL("Post", POST_SCHEMA);
 module.exports = POST;
